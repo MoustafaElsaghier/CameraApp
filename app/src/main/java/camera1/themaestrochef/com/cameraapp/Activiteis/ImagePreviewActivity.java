4ddcf7ffe1098e613ad669aa63e213372dd91a48
EL_Saghier <thedreamereng@gmail.com>
2018-09-18 06:06:23 +0200

    create the image preview screen

---
+++ app/src/main/java/camera1/themaestrochef/com/cameraapp/Activiteis/ImagePreviewActivity.java
@@ -0,0 +1,21 @@
+package camera1.themaestrochef.com.cameraapp.Activiteis;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+import butterknife.BindView;
+import camera1.themaestrochef.com.cameraapp.R;
+import camera1.themaestrochef.com.cameraapp.Utilities.UiUtilies;
+
+public class ImagePreviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_preview);
+
+    }
+}
